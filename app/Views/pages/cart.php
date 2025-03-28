e10b4417d51ee03053d6574032fcd88dc1050413
Muhammad Fajar Ilham <mfilham06@gmail.com>
2025-03-28 17:42:07 +0700

    update cart

--- app/Views/pages/cart.php
+++ app/Views/pages/cart.php
@@ -2,9 +2,9 @@
 
 <?= $this->section('content'); ?>
 
-    <div class="container bg-dark-subtle mt-5" style="min-height: 80vh">
+    <div class="container mt-5" style="min-height: 80vh">
         <div class="row">
-            <div class="col-lg-8 bg-info">
+            <div class="col-lg-8">
                 <!-- card -->
                 <div class="card mb-3">
                     <div class="card-header">
@@ -21,10 +21,21 @@
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                <h5 class="card-title">koleksi mobil mobilan porsche, dodge, ford mustang dan corvette
+                                    sangat teramat langka</h5>
+                                <p class="card-text fw-bold">Rp200.000</p>
+                                <div class="row gx-5">
+                                    <div class="col-auto">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-trash"></i></button>
+                                    </div>
+                                    <div class="col-auto row border rounded px-0">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-dash"></i>
+                                        </button>
+                                        <p class="col-auto my-auto">2</p>
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
                                 </p>
                             </div>
                         </div>
@@ -45,10 +56,21 @@
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                <h5 class="card-title">koleksi mobil mobilan porsche, dodge, ford mustang dan corvette
+                                    sangat teramat langka</h5>
+                                <p class="card-text fw-bold">Rp200.000</p>
+                                <div class="row gx-5">
+                                    <div class="col-auto">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-trash"></i></button>
+                                    </div>
+                                    <div class="col-auto row border rounded px-0">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-dash"></i>
+                                        </button>
+                                        <p class="col-auto my-auto">2</p>
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
                                 </p>
                             </div>
                         </div>
@@ -69,10 +91,21 @@
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                <h5 class="card-title">koleksi mobil mobilan porsche, dodge, ford mustang dan corvette
+                                    sangat teramat langka</h5>
+                                <p class="card-text fw-bold">Rp200.000</p>
+                                <div class="row gx-5">
+                                    <div class="col-auto">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-trash"></i></button>
+                                    </div>
+                                    <div class="col-auto row border rounded px-0">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-dash"></i>
+                                        </button>
+                                        <p class="col-auto my-auto">2</p>
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
                                 </p>
                             </div>
                         </div>
@@ -93,18 +126,35 @@
                         </div>
                         <div class="col-9">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                <h5 class="card-title">koleksi mobil mobilan porsche, dodge, ford mustang dan corvette
+                                    sangat teramat langka</h5>
+                                <p class="card-text fw-bold">Rp200.000</p>
+                                <div class="row gx-5">
+                                    <div class="col-auto">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-trash"></i></button>
+                                    </div>
+                                    <div class="col-auto row border rounded px-0">
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-dash"></i>
+                                        </button>
+                                        <p class="col-auto my-auto">2</p>
+                                        <button class="col-auto btn btn-link py-0 px-1 fs-5"><i class="bi bi-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 mt-3 mt-lg-0 bg-danger">
-                <button class="btn btn-primary w-100 my-3 sticky-lg-top" style="top: 10vh">Beli</button>
+            <div class="col-lg-4 mt-3 mt-lg-0">
+                <div class="sticky-lg-top my-3 border rounded p-2 bg-body-tertiary" style="top: 10vh">
+                    <div class="row">
+                        <p class="text-body fw-bold fs-5 col-auto">Total: </p>
+                        <p class="text-body fw-bold fs-5 col-auto">Rp200.000 </p>
+                    </div>
+                    <button class="btn btn-primary w-100 my-3">Beli</button>
+                </div>
             </div>
         </div>
     </div>
