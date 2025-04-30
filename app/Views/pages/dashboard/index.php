c3edc85c2de663eb0bed910a1d566d11a599b624
Muhammad Fajar Ilham <mfilham06@gmail.com>
2025-05-01 02:41:46 +0700

    refactor(assets): menyesuaikan direktori sb-admin-2
    struktur direktori sb admin disesuaikan seperti aslinya, aset asli dan sb admin dibedakan

--- app/Views/pages/dashboard/index.php
+++ app/Views/pages/dashboard/index.php
@@ -12,13 +12,13 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('public/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('public/assets/sb-admin-2/vendor/fontawesome-free/css/all.css') ?>" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url('/css/sb-admin-2.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/sb-admin-2/css/sb-admin-2.css') ?>" rel="stylesheet">
 
 </head>
 
@@ -738,21 +738,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/sb-admin-2/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="assets/sb-admin-2/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="assets/sb-admin-2/js/sb-admin-2.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="assets/sb-admin-2/vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="assets/sb-admin-2/js/demo/chart-area-demo.js"></script>
+    <script src="assets/sb-admin-2/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
